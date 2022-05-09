0e0d9518e0705c4b93329cb311176d29247072f1
rujanm <rujan.m@gmail.com>
2022-05-08 23:01:04 -0500

    commit

---
+++ src/Assignment1_1.java
@@ -0,0 +1,8 @@
+public class Assignment1_1 {
+    public static void main(String[] args) {
+        System.out.println("Rujani Mahmud");
+        System.out.println("Full Stack Java Course");
+        System.out.println("Dushanbe, Tajikistan");
+        System.out.println("Oreo & Milk");
+    }
+}
---
+++ src/Assignmewnt1_2.java
@@ -0,0 +1,10 @@
+public class Assignmewnt1_2 {
+    
+    public static void main(String[] args) {
+        String logo = "     J    A    V     V     A \n" +
+                      "     J   A A    V   V     A A \n" +
+                      "J    J  AAAAA    V V     AAAAA \n" +
+                      " J  J  A     A    V     A     A";
+        System.out.println(logo);
+    }
+}
